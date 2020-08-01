e7e0f98290864933e78747e08779e0ec9db8d542
yofio <d.y215jump@i.softbank.jp>
2020-08-01 14:50:07 +0900

    q

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,6 +14,7 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2.3.1です
 
+
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
                   keys: ['~/.ssh/deploytest.pem'] 
